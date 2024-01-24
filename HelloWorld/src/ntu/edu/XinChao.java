0992cd6df9a0cc0109013e33d5f7381636006fcf
MinhKy2k1 <lyminhky29401@gmail.com>
2024-01-24 19:47:08 +0700

    Update XinChao.java

--- HelloWorld/src/ntu/edu/XinChao.java
+++ HelloWorld/src/ntu/edu/XinChao.java
@@ -13,7 +13,7 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Lần đầu học Java");
+		System.out.println("Lần đầu học Java6");
 		System.out.println("Sửa dòng trên");
 	}
 
