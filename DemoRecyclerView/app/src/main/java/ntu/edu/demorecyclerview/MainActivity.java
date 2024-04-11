42f2142550f9a35f79c11fed74ba1e2e856047c4
MinhKy2k1 <lyminhky29401@gmail.com>
2024-04-11 12:20:58 +0700

    RecyclerView
    Khởi tạo và cài đặt tại activity hiển thị

--- DemoRecyclerView/app/src/main/java/ntu/edu/demorecyclerview/MainActivity.java
+++ DemoRecyclerView/app/src/main/java/ntu/edu/demorecyclerview/MainActivity.java
@@ -7,12 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    CitiesAdapter citiesAdapter;
+    ArrayList<Cities> recyclerViewData;
+    RecyclerView recyclerViewCities;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    ArrayList<>
 }
