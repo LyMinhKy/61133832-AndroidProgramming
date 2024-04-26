69d00715682d831685f170237d87e03bc2f3b788
MinhKy2k1 <lyminhky29401@gmail.com>
2024-04-26 21:01:09 +0700

    Chưa xong phần MainActivity

--- BottomNavigation/app/src/main/java/ky/lm/bottomnavigation/MainActivity.java
+++ BottomNavigation/app/src/main/java/ky/lm/bottomnavigation/MainActivity.java
@@ -1,20 +1,52 @@
 package ky.lm.bottomnavigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnItemSelectedListener{
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
 
+    }
+    HomeFragment homeFragment = new HomeFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.bottomNavigationView,homeFragment).commit();
+                return true;
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.bottomNavigationView,profileFragment).commit();
+                return true;
+            case R.id.setting:
+                getSupportFragmentManager().beginTransaction().replace(R.id.bottomNavigationView,settingsFragment).commit();
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
