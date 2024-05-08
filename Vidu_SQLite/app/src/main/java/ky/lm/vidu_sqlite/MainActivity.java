f34df9680e6576f1801fa0260ef413ee28aabb7f
MinhKy2k1 <lyminhky29401@gmail.com>
2024-05-08 09:56:22 +0700

    Khai báo các biến

--- Vidu_SQLite/app/src/main/java/ky/lm/vidu_sqlite/MainActivity.java
+++ Vidu_SQLite/app/src/main/java/ky/lm/vidu_sqlite/MainActivity.java
@@ -1,6 +1,9 @@
 package ky.lm.vidu_sqlite;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    //Khai báo các biến Database
+    String DbPathSuffix= "/databases/";//thư mục chứa csdl
+    SQLiteDatabase database=null;//khai báo tên csdl
+    String DbName="qlsach.db";//tên file chứa csdl
+    //Khai báo ListView
+    ListView lv;
+    ArrayList<String> ds;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
