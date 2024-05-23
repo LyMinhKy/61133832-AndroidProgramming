54c6faec71842f88d842f3bb7cd581112b2d2077
MinhKy2k1 <lyminhky29401@gmail.com>
2024-05-23 19:20:00 +0700

    On thi

--- BaiOnTap/app/src/main/java/ky/lm/baiontap/MainActivity.java
+++ BaiOnTap/app/src/main/java/ky/lm/baiontap/MainActivity.java
@@ -1,24 +1,60 @@
 package ky.lm.baiontap;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private BottomNavigationView bottomNavigationView;
+    private  FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        frameLayout = findViewById(R.id.flFragment);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem Item) {
+                int itemId=Item.getItemId();
+                if(itemId==R.id.home){
+                    loadFragment(new Home_Fragment(),false );
+                }else if(itemId==R.id.cau1){
+                    loadFragment(new Cau1_Fragment(),false );
+                }else if(itemId==R.id.cau2){
+                    loadFragment(new Cau2_Fragment(),false );
+                }else if(itemId==R.id.cau3){
+                    loadFragment(new Cau3_Fragment(),false );
+                }else{
+                    loadFragment(new Cau4_Fragment(),false );
+                }
+                return true;
+            }
         });
+        loadFragment(new Home_Fragment(), true);
+    }
+    private void loadFragment(Fragment fragment, boolean isAppInitialized){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if(isAppInitialized){
+            fragmentTransaction.add(R.id.flFragment, fragment);
+        }else{
+            fragmentTransaction.replace(R.id.flFragment, fragment);
+        }
+        fragmentTransaction.commit();
     }
 }
