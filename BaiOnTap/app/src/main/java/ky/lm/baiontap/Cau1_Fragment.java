54d6dd1e64e9f91a36c04a475558d427bef30933
MinhKy2k1 <lyminhky29401@gmail.com>
2024-05-27 21:29:53 +0700

    cau 1

--- BaiOnTap/app/src/main/java/ky/lm/baiontap/Cau1_Fragment.java
+++ BaiOnTap/app/src/main/java/ky/lm/baiontap/Cau1_Fragment.java
@@ -7,11 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Cau1_Fragment extends Fragment {
 
-
+    EditText Soa;
+    EditText Sob;
+    EditText KQ;
+    Button nutCong,nutTru,nutNhan,nutChia;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
