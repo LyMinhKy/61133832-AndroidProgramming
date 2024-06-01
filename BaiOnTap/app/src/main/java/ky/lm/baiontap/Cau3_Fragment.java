637f30461e05ebe1192b9b972c1c36637b8f2dca
MinhKy2k1 <lyminhky29401@gmail.com>
2024-06-01 21:37:24 +0700

    cau3

--- BaiOnTap/app/src/main/java/ky/lm/baiontap/Cau3_Fragment.java
+++ BaiOnTap/app/src/main/java/ky/lm/baiontap/Cau3_Fragment.java
@@ -1,5 +1,8 @@
 package ky.lm.baiontap;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +10,37 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
 
 
 public class Cau3_Fragment extends Fragment {
+    //Khai báo các biến Database
+    String DbPathSuffix= "/databases/";//thư mục chứa csdl
+    SQLiteDatabase database=null;//khai báo tên csdl
+    String DbName="qlsach.db";//tên file chứa csdl
+    //Khai báo ListView
+    ListView lv;
+    ArrayList<String> ds;
+    ArrayAdapter<String> adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cau3_, container, false);
+        View viewcau3 = inflater.inflate(R.layout.fragment_cau3_, container, false);
+        lv=viewcau3.findViewById(R.id.lv);
+        ds = new ArrayList<>();
+        adapter = new ArrayAdapter<>(viewcau3.getContext(), android.R.layout.simple_list_item_1,ds);
+        lv.setAdapter(adapter);
+        //Gọi hàm tiến hành copy csdl từ Assets vào thư mục db trong bo nhớ cài đặt ứng dụng
+        //processCopy();
+        database = getActivity().openOrCreateDatabase("qlsach.db",MODE_PRIVATE,null);
+        return viewcau3;
+
+         
     }
+
 }
